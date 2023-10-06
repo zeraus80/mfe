0f23d62b3d17f8f0c41b063832d73cc97e1db42b
Javier Navarro <javier.navarros@perficient.com>
2023-10-06 14:52:58 -0500

    Added styling

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
+    </BrowserRouter>
   );
 };
