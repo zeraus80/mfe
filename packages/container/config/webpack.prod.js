958c5cc60a0d3afdb34ada53feff0ecf7ce96539
Javier Navarro <javier.navarros@perficient.com>
2023-10-06 10:45:25 -0500

    Added config comment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -5,6 +5,7 @@ const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
+// Production configuration
 const prodConfig = {
     mode: 'production',
     output: {
