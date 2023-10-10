9be1ae82e0082cd6b564e4802ad6c6073ad16801
Javier Navarro <javier.navarros@perficient.com>
2023-10-10 07:12:10 -0500

    Added auth subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Progress from './components/Progress';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
-    <StylesProvider generateClassName={generateClassName}>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 
 // Mount function to start up the app
-const mount = (el) => ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+    const history = defaultHistory ?? createMemoryHistory({
+        initialEntries: [initialPath]
+    });
+    onNavigate && history.listen(onNavigate);
+
+    ReactDOM.render(<App history={history}/>, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+              history.push(nextPathname);
+            }
+        }
+    };
+};
 
 // If we are in development and in isolation,
 // call mount inmmediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
-    devRoot && mount(devRoot);
+    devRoot && mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 // We are running through container
